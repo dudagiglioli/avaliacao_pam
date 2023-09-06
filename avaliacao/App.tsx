85c638a4ecb304766b8daf93bf324f4022784336
dudagiglioli <dudaguilice@gmail.com>
2023-09-06 15:34:00 -0300

    css :construction:

--- avaliacao/App.tsx
+++ avaliacao/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import Avaliacao from './carrinhoApp/pages/Avaliacao';
+import { useFonts,
+  Satisfy_400Regular
+} from '@expo-google-fonts/satisfy';
 
 export default function App() {
+
+const[fonteCarregada] = useFonts({
+  "SatisfyRegular" : Satisfy_400Regular
+})
+  if(!fonteCarregada){
+    return null
+  }
+
   return (
   <SafeAreaView>
 <StatusBar/>
--- avaliacao/carrinhoApp/pages/Avaliacao/index.tsx
+++ avaliacao/carrinhoApp/pages/Avaliacao/index.tsx
@@ -1,23 +1,68 @@
-import React from "react";
-import {Text, StyleSheet, Dimensions } from 'react-native';
+import React from "react-native";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 import topo from "../../../assets/topo.png";
+import logo from "../../../assets/logo.png"
 
 const width = Dimensions.get("screen").width;
 
 export default function Avaliacao(){
-    return(
-        <>
-     <Image source={topo} style={estilos.topo}/>
-<Text>  
-la floristería del amor
-</Text>
-        </>
-    );
-}
+    return<>
+
+     <Image style={estilos.topo} source={topo}/>
+     <Text style={estilos.titulo}>La Floristería Del Amor</Text>
+
+     <View style={estilos.inicio}>
+
+        <Image style={estilos.logo} source={logo}/>
+        <Text style={estilos.text1}>A floricultura ideal para o melhor presente!</Text>
+
+     </View>
+
+    
+
+        <Text style={estilos.text2}>A floricultura ideal para encontrar o presente do seu amor, 
+                                    com diversas opções de cestas, flores e significados para presentear.</Text>
+
+    
+        </>}
 
 const estilos = StyleSheet.create({
     topo: {
         width: "100%",
         height: (578 / 768) * width,
     },
+
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        color: "black",
+        textAlign: "center",
+        fontSize: 30,
+        lineHeight: 26,
+        padding: 26,
+        fontFamily: "SatisfyRegular"
+      },
+    
+    inicio: {
+        flexDirection:"row",
+        paddingVertical: 12
+      },
+    
+    text1:{
+        fontSize: 20,
+        lineHeight: 42,
+        color: "#464646",
+        fontFamily:"SatisfyRegular"
+    },
+
+    logo: {
+        width:52,
+        height:52
+        },
+
+    text2: {
+            color: "#A3A3A3",
+            fontFamily: "MontserratItalic"
+          },
+      
 })
